64703885892da9a907c6704144257f793f03cdbd
lna <arfanmaulana7683@gmail.com>
2026-04-27 21:38:47 +0700

    Update and rename data.js to gallery-data.js

---
+++ gallery-data.js
@@ -0,0 +1,137 @@
+/* =========================================
+   gallery-data.js — Data foto untuk Gallery
+   
+   Cara tambah foto:
+   1. Upload ke Imgur → kanan klik → Copy image address
+   2. Tambah objek baru di array PHOTOS di bawah
+   ========================================= */
+
+const PHOTOS = [
+  {
+    src:     "https://i.imgur.com/1UrF77M.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/jUNk6Hz.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/AhW9fJ9.jpeg",
+    caption: "Me & Friend"
+  },
+  {
+    src:     "https://i.imgur.com/45U6DUw.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/tivxTrH.jpeg",
+    caption: "Me & Friend"
+  },
+  {
+    src:     "https://i.imgur.com/HrNBrev.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/42Ugv17.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/TAnxnxA.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/J6M3NFf.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/RowshPg.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/0pjMz0K.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/vHGrkYc.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/VIA2Q9W.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/H7rSOXQ.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/I0hKQQQ.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/iVJLxAV.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/5AsZX7j.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/3sQo0fE.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/DPjk6WO.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/3ft28MZ.jpeg",
+    caption: "Me & Moms"
+  },
+  {
+    src:     "https://i.imgur.com/tlq4glO.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/F8GnnVC.jpeg",
+    caption: "Me & Cats"
+  },
+  {
+    src:     "https://i.imgur.com/EQve6lp.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/kxnRNEV.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/qOCcDmo.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/fxl89rW.jpeg",
+    caption: "Me & Friend"
+  },
+  {
+    src:     "https://i.imgur.com/eubBTHi.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/CKap8KN.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/o0YSxGr.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/pzMs926.jpeg",
+    caption: "Me"
+  },
+  {
+    src:     "https://i.imgur.com/8s5OOJE.jpeg",
+    caption: "Me"
+  }
+];
+
+// Preview di index.html (jumlah foto yang ditampilkan di halaman depan)
+const GALLERY_PREVIEW_LIMIT = 6;
