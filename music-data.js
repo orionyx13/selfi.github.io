b428bd3b696d31d8a520b61661d1fa397e6b9c37
lna <arfanmaulana7683@gmail.com>
2026-04-29 21:02:58 +0700

    Update music-data.js

--- music-data.js
+++ music-data.js
@@ -1,9 +1,5 @@
 /* =========================================
    music-data.js — Data lagu untuk Music Player
-   Cara tambah lagu:
-   1. Upload .mp3 ke GitHub repo / hosting
-   2. Klik Raw → copy URL
-   3. Tambah objek baru di array SONGS di bawah
    ========================================= */
 
 const SONGS = [
@@ -29,32 +25,8 @@ const SONGS = [
     title:  "Sedia Aku Sebelum Hujan",
     artist: "idgitaf",
     dur:    "3:50",
-    src:    "https://res.cloudinary.com/deqjuasvw/video/upload/v1777471152/Sedia_Aku_Sebelum_Hujan_b5s6xa.mp3"
-  },
-  {
-    title:  "Lover",
-    artist: "Taylor Swift",
-    dur:    "3:41",
-    src:    "https://raw.githubusercontent.com/USERNAME/REPO/main/music/lover.mp3"
-  },
-  {
-    title:  "Aesthetic",
-    artist: "Tollan Kim",
-    dur:    "3:30",
-    src:    "https://raw.githubusercontent.com/USERNAME/REPO/main/music/aesthetic.mp3"
-  },
-  {
-    title:  "Kangen",
-    artist: "Dewa 19",
-    dur:    "4:12",
-    src:    "https://raw.githubusercontent.com/USERNAME/REPO/maisic/kangen.mp3"
-  },
-  {
-    title:  "Hati-Hati di Jalan",
-    artist: "Tulus",
-    dur:    "3:58",
-    src:    "https://raw.githubusercontent.com/USERNAME/REPO/main/music/hati-hati-di-jalan.mp3"
-  },
+    src:    "https://res.cloudinary.com/deqjuasvw/audio/upload/v1777471152/Sedia_Aku_Sebelum_Hujan_b5s6xa.mp3"
+  }
 ];
 
 // Untuk preview di index.html (jumlah lagu yang ditampilkan)
